afa418d66c4850d5db56db6a11bc0f756f7f8e2b
w3schools-test <bhadeshiyajay12@gmail.com>
2025-08-26 18:33:52 +0530

    Change

--- Project/LMSProjectAPI/Controllers/CourseAPIController.cs
+++ Project/LMSProjectAPI/Controllers/CourseAPIController.cs
@@ -19,6 +19,7 @@ public class CourseAPIController : ControllerBase
     }
     #endregion
 
+
     #region GetAllCourse
     [HttpGet]
     public async Task<ActionResult<IEnumerable<object>>> GetAllCourse()
